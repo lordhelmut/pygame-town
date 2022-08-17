546f4688f5b29f8fb15a7088dc0d735182e23c39
lordhelmut <lordhelmut@gmail.com>
2022-08-17 16:13:56 -0700

    setup sprites

--- src/level.py
+++ src/level.py
@@ -12,6 +12,20 @@ logging.basicConfig(
 
 
 class Level:
+    def __init__(self):
+        logging.info('The game has started')
+        # get the surface for the display
+        self.display_surface = pygame.display.get_surface()
+
+        # groups for the sprites
+        self.all_sprites = pygame.sprite.Group()
+
+
     def run(self, dt):
         # print('The game is running')
-        logging.info('The game is running')
+        logging.debug('The game is running')
+
+        # remove previous screens and draw new one
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update()
