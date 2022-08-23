633d8d0ad968647185ec4f5667341ce39d29a1e5
lordhelmut <lordhelmut@gmail.com>
2022-08-23 15:18:40 -0700

    player movement and idle

--- src/player.py
+++ src/player.py
@@ -24,7 +24,7 @@ class Player(pygame.sprite.Sprite):
         self.speed = 200
 
     def import_assets(self):
-        logging.debug('Loading animations')
+        logging.info('Loading assets')
 
         self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
                            'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
@@ -38,35 +38,62 @@ class Player(pygame.sprite.Sprite):
             if (LOGGINGOPTS == 'DEBUG'):
                 logging.debug(f'self.animations: {self.animations}')
 
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+        self.image = self.animations[self.status][int(self.frame_index)]
+        if (LOGGINGOPTS == 'DEBUG'):
+            logging.debug(f'self image animation: {self.image}')
+
     def input(self):
 
         # get the key presses
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-            if (LOGGINGOPTS == 'DEBUG'):
-                logging.debug('up arrow pressed')
             self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
+            # set the correct animation surface
+            self.status = 'up'
             if (LOGGINGOPTS == 'DEBUG'):
-                logging.debug('down arrow pressed')
+                logging.debug(f'up arrow pressed. self.status: {self.status}')
+        elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
+            if (LOGGINGOPTS == 'DEBUG'):
+                logging.debug('down arrow pressed. self.status: {self.status}')
         else:
             # this keeps the character from moving indefinitely
             self.direction.y = 0
+
         if keys[pygame.K_RIGHT]:
-            if (LOGGINGOPTS == 'DEBUG'):
-                logging.debug('right arrow pressed')
             self.direction.x = 1
-        elif keys[pygame.K_LEFT]:
+            self.status = 'right'
             if (LOGGINGOPTS == 'DEBUG'):
-                logging.debug('left arrow pressed')
+                logging.debug(
+                    'right arrow pressed. self.status: {self.status}')
+        elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
+            if (LOGGINGOPTS == 'DEBUG'):
+                logging.debug('left arrow pressed. self.status: {self.status}')
         else:
             # this keeps the character from moving indefinitely
             self.direction.x = 0
         if (LOGGINGOPTS == 'DEBUG'):
-            logging.debug(f'direction: {self.direction}')
+            logging.debug(
+                f'direction: {self.direction}. self.status: {self.status}')
+
+    def get_status(self):
+        # if player is not moving, add "_idle" to status
+        movement = self.direction.magnitude()
+        if movement == 0:
+            # split the string so that the first part
+            # always returns the root file path
+            # e.g. "up" such that "up_idle" will also return "up"
+            self.status = self.status.split('_')[0] + '_idle'
+        if (LOGGINGOPTS == 'DEBUG'):
+            logging.debug(f'movement: {movement} self.status: {self.status}')
 
     def move(self, dt):
         # vector math, needs to be a positive number
@@ -86,4 +113,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
