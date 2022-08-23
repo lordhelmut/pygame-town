25026e819789e0d794066de048a98bd3f25cd89e
lordhelmut <lordhelmut@gmail.com>
2022-08-23 16:11:10 -0700

    timer class

---
+++ src/timer.py
@@ -0,0 +1,28 @@
+import pygame
+import logging
+
+# create a class to reuse time duration functions
+class Timer:
+    def __init__(self, duration, func=None):
+        self.duration = duration
+        self.func = func
+        self.start_time = 0
+        self.active = False
+
+    def activate(self):
+        self.active = True
+        # set the timer as a marker when it was instantiated
+        self.start_time = pygame.time.get_ticks()
+
+    def deactivate(self):
+        self.active = False
+        self.start_time = 0
+
+    def update(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time >= self.duration:
+            # duh - turn it off
+            self.deactivate()
+            # if the function exists, instantiate it
+            if self.func:
+                self.func()
