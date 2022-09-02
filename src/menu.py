c6b0cac7f4a8eaf2d9e3e5bd6ccf43396adf1702
lordhelmut <lordhelmut@gmail.com>
2022-09-02 13:54:31 -0700

    item menu

--- src/menu.py
+++ src/menu.py
@@ -12,6 +12,22 @@ class Menu:
         font_path = 'font/LycheeSoda.ttf'
         self.font = pygame.font.Font(font_path, 30)
 
+        # options
+        self.width = 400
+        self.space = 10  # between elements
+        self.heading = 8
+
+        self.trader_options = list(
+            self.player.item_inventory.keys()) + list(self.player.seed_inventory.keys())
+        self.sell_border = len(self.player.item_inventory) - 1
+        self.setup()
+
+    def setup(self):
+        self.text_surfs = []
+        for item in self.trader_options:
+            text_surf = self.font.render(item, False, 'Black')
+            self.text_surfs.append(text_surf)
+
     def input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
@@ -19,4 +35,6 @@ class Menu:
 
     def update(self):
         self.input()
-        self.display_surface.blit(pygame.Surface((1000,1000)),(0,0))
+        # use the item inventory as the menu options
+        for text_index, text_surf in enumerate(self.text_surfs):
+            self.display_surface.blit(text_surf, (100, text_index*50))
